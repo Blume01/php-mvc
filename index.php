bb1203373211c7985664622326b5426b37e3e438
Blume <32483794+Blume01@users.noreply.github.com>
2024-11-10 10:50:53 -0300

    first commit

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $router = new Router();
 $router->add('/', [new AuthController(), 'index']);
 $router->add('/login', [new AuthController(), 'login']);
 $router->add('/logout', [new AuthController(), 'logout']);
-
 $router->add('/user/{id}', [new UserController(), 'show']);
 $router->add('/home', [new HomeController(), 'index']);
 
